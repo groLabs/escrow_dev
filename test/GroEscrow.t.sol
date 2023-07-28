53fcdbf116aa1c929cc183608b305db41fd5180d
SHAKOTN <shakotn@protonmail.com>
2023-07-28 14:20:40 +0300

    [tests] - add more tests for error cases, also: - Add coverage GH action

--- test/GroEscrow.t.sol
+++ test/GroEscrow.t.sol
@@ -10,6 +10,10 @@ contract TestGroEscrow is BaseFixture {
         super.setUp();
     }
 
+    //////////////////////////////////////////////////////////////////////////////
+    ///////                       Happy cases                              ///////
+    //////////////////////////////////////////////////////////////////////////////
+    /// @notice Simply test that a deposit can be made and that the position is created
     function testSimpleDeposit(uint96 depositAmnt) public {
         vm.assume(depositAmnt > 1e6);
         // Give alice some USDC
@@ -92,6 +96,42 @@ contract TestGroEscrow is BaseFixture {
         assertTrue(claimed);
     }
 
+    //////////////////////////////////////////////////////////////////////////////
+    ///////                       Error cases                              ///////
+    //////////////////////////////////////////////////////////////////////////////
+
+    function testDepositZeroAddr() public {
+        vm.startPrank(alice);
+        vm.expectRevert("ZERO_ADDRESS");
+        escrow.deposit(address(0), bob, 100e6, 182 days);
+
+        vm.expectRevert("ZERO_ADDRESS");
+        escrow.deposit(address(usdc), address(0), 100e6, 182 days);
+        vm.stopPrank();
+    }
+
+    function testDepositZeroAmnt() public {
+        vm.startPrank(alice);
+
+        vm.expectRevert("ZERO_AMOUNT");
+        escrow.deposit(address(usdc), bob, 0, 182 days);
+        vm.stopPrank();
+    }
+
+    function testClaimZeroAddr() public {
+        vm.startPrank(alice);
+        vm.expectRevert("ZERO_ADDRESS");
+        bytes memory signatures;
+        escrow.claim(address(0), bob, alice, 0, signatures);
+
+        vm.expectRevert("ZERO_ADDRESS");
+        escrow.claim(address(usdc), address(0), alice, 0, signatures);
+
+        vm.expectRevert("ZERO_ADDRESS");
+        escrow.claim(address(usdc), bob, address(0), 0, signatures);
+        vm.stopPrank();
+    }
+
     function testDepositClaimTooEarly(
         uint128 depositAmnt,
         uint224 depositLength
@@ -141,5 +181,45 @@ contract TestGroEscrow is BaseFixture {
         );
         escrow.claim(address(usdc), jill, jake, 0, signatures);
         vm.stopPrank();
+    }
+
+    function testClaimInvalidSig() public {
+        uint256 depositAmnt = 1000e6;
+        uint256 depositLength = 100;
+        (address jake, uint256 jakeKey) = makeAddrAndKey("1337");
+        vm.label(jake, "Jake");
+        (address jill, uint256 jillKey) = makeAddrAndKey("1338");
+        vm.label(jill, "Jill");
+
+        uint256 jillBalanceSnapshot = usdc.balanceOf(jill);
+        assertEq(jillBalanceSnapshot, 0);
+        vm.startPrank(jake);
+        usdc.faucet(depositAmnt);
+        usdc.approve(address(escrow), depositAmnt);
+        // Jake wants to give X USDC to Jill and put it into escrow
+        escrow.deposit(address(usdc), jill, depositAmnt, depositLength);
+        vm.stopPrank();
+        vm.warp(block.timestamp + depositLength + 1);
+        // Jake signs the claim message
+        (uint8 v, bytes32 r, bytes32 s) = signClaimMessage(
+            address(usdc),
+            jill,
+            jake,
+            0,
+            jakeKey
+        );
+        // Jill agrees with Jake and signs the message but it contains invalid nonce
+        (uint8 v2, bytes32 r2, bytes32 s2) = signClaimMessage(
+            address(usdc),
+            jill,
+            jake,
+            1,
+            jillKey
+        );
+        bytes memory signatures = packSignatures(v, r, s, v2, r2, s2);
+        vm.startPrank(jill);
+        // As Jill signed invalid message, she should get reverted
+        vm.expectRevert(abi.encodeWithSelector(Errors.InvalidSig.selector));
+        escrow.claim(address(usdc), jill, jake, 0, signatures);
     }
 }
